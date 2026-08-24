7ff990bd9e68c0be2443f16b8f49c91fd126489d
Abd-Alrhman Khashashneh <100373897+Abdalrhmankhashashneh@users.noreply.github.com>
2026-08-24 03:02:24 +0300

    fix file path issue

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/beautiful-portfolio/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
